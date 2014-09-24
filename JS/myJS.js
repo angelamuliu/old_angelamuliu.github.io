cc9983d458b2f0a632b11fbe94f6f6b2a290c9ae
Angela Liu <angelamuliu@gmail.com>
2014-09-24 18:07:48 -0400

    Added changing scrollbar

--- JS/myJS.js
+++ JS/myJS.js
@@ -1,1 +1,12 @@
 
+$(document).ready( function() {
+
+	// Change color of scroll as I go down
+	$(window).scroll( function() {
+		var scrollposition = $(window).scrollTop();
+		$(".nav-row")[0].style.background="rgba(0,0,0," + scrollposition/120 + ")";
+	})
+
+
+
+})

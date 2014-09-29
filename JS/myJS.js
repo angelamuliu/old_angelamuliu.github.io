e5fe0f6a19d0a4f459cebe12aace036b41d58b6e
Angela Liu <angelamuliu@gmail.com>
2014-09-29 02:12:38 -0400

    Fixed breakpoint for nav. Hover over gallery for title. Fixed padding issues with hover.

--- JS/myJS.js
+++ JS/myJS.js
@@ -7,9 +7,17 @@ $(document).ready( function() {
 		$(".nav-row")[0].style.background="rgba(0,0,0," + scrollposition/120 + ")";
 	})
 
-
+	// A different greeting every time you come back! : )
 	var greetings= ["Aloha!", "Welcome!", "Salutations!", "Wassup?", "Howdy!", "Ahoy!"];
 	var randomnumber = Math.ceil(Math.random()*5);
 	$("#greet").html("<h1>"+greetings[randomnumber]+"</h1>");
+
+	$(".project").hover(
+		function() {
+			$(this).find(".project_name").slideDown(400);
+		}, function() {
+			$(this).find(".project_name").slideUp(400);
+		})
+
 
 })

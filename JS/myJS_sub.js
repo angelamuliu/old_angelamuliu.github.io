beb4015ad2db517a1c4dc5d8ededfe2d158e201e
Angela Liu <angelamuliu@gmail.com>
2014-11-03 22:19:35 -0500

    Added isotope to subpage template

--- JS/myJS_sub.js
+++ JS/myJS_sub.js
@@ -14,20 +14,11 @@ $(document).ready( function() {
 	// Works for touch events too
 	$('.nav-toggle').on({ 'touchstart' : navtoggle });
 
-	// A different greeting every time you come back! : )
-	var greetings= ["Aloha!", "Welcome!", "Salutations!", "Wassup?", "Howdy!", "Ahoy!"];
-	var randomnumber = Math.ceil(Math.random()*5);
-	$("#greet").html("<h1>"+greetings[randomnumber]+"</h1>");
-
-	// Filter is called on a button click, button has id. Anything within
-	// project area with a matching class name is shown, nonmatches are hidden
-	$("button.filter").click( function() {
-		$("button.filter").removeClass("selected_filter");
-		$(this).addClass("selected_filter");
-		var filterterm = "." + $(this).attr("id");
-		var $container = $("#gallery");
-		$container.isotope({filter: filterterm});
-	})
+	// fancybox gallery widget, used in subpages
+	$(".fancybox").fancybox({
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
 
 	// --------------------------------------------
 	// HELPERS
